b70dced6028698c1cefe60ecdcd1761f6bbbcf18
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-04 04:30:37 +0900

    UserRepositoryを仮作成

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import "gorm.io/gorm"
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create() error {
+	// 今は仮
+	return nil
+}
