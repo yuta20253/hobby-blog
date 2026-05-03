79129b287901d36a389c03ecd92c3001963a491d
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-04 04:05:28 +0900

    handlerを分割

--- backend/internal/handler/handler.go
+++ backend/internal/handler/handler.go
@@ -6,16 +6,8 @@ type Handlers struct {
 	Auth *AuthHandler
 }
 
-type AuthHandler struct {
-	db *gorm.DB
-}
-
 func NewHandlers(db *gorm.DB) *Handlers  {
 	return &Handlers {
 		Auth: NewAuthHandler(db),
 	}
-}
-
-func NewAuthHandler(db *gorm.DB) *AuthHandler {
-	return &AuthHandler{db: db}
 }
