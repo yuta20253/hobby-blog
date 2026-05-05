41a0f32d8914cf22846bc3c26d4e66e082b0bfe3
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-06 02:14:08 +0900

    Modelの修正

--- backend/internal/model/category.go
+++ backend/internal/model/category.go
@@ -3,7 +3,6 @@ package model
 import "time"
 
 type Category struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement"`
+	gorm.Model
 	Name      string    `gorm:"type:varchar(50);unique;not null"`
-	CreatedAt time.Time `gorm:"not null"`
 }
--- backend/internal/model/post.go
+++ backend/internal/model/post.go
@@ -3,15 +3,13 @@ package model
 import "time"
 
 type Post struct {
-	ID         uint      `gorm:"primaryKey;autoIncrement"`
+	gorm.Model
 	UserID     uint      `gorm:"not null"`
 	CategoryID uint      `gorm:"not null"`
 	Title      string    `gorm:"type:varchar(255);not null"`
 	Content    string    `gorm:"type:text;not null"`
 	Status     string    `gorm:"type:enum('draft','published');not null;default:'draft'"`
-	CreatedAt  time.Time `gorm:"not null"`
-	UpdatedAt  time.Time `gorm:"not null"`
 
-	User       User     `gorm:"foreignKey:UserID;references:ID"`
-	Category   Category `gorm:"foreignKey:CategoryID;references:ID"`
+	User     User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Category Category `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
 }
