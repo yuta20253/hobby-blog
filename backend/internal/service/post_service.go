586ea75ee68cf92bf9f3e9f38d47e53488342193
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-06 03:20:33 +0900

    修正もれ

--- backend/internal/service/post_service.go
+++ backend/internal/service/post_service.go
@@ -27,7 +27,7 @@ type PostDetailResponse struct {
 	ID uint `json:"id"`
 	Title string `json:"title"`
 	Content string `json:"content"`
-	User UserResponse `json:"user"`
+	User PostUserResponse `json:"user"`
 	Category CategoryResponse `json:"category"`
 }
 
