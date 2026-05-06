1059fa165396958346d474f57351eeb9d85cdd22
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-06 23:45:11 +0900

    カラムの型修正

--- backend/internal/model/post.go
+++ backend/internal/model/post.go
@@ -1,6 +1,9 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"hobby-blog/internal/domain/post"
+)
 
 type Post struct {
 	gorm.Model
@@ -8,7 +11,7 @@ type Post struct {
 	CategoryID uint      `gorm:"not null"`
 	Title      string    `gorm:"type:varchar(255);not null"`
 	Content    string    `gorm:"type:text;not null"`
-	Status     string    `gorm:"type:enum('draft','published');not null;default:'draft'"`
+	Status     post.Status    `gorm:"type:varchar(20);not null;default:'draft'"`
 
 	User     User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Category Category `gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
