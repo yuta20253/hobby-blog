a30e176328f96d1d69ff560392775e172bb4bd90
やすす <yuta20253@yasusunoMBP.lan>
2026-06-20 23:28:39 +0900

    SignUpRequestとLoginRequestにバリデーションルール追加

--- backend/internal/dto/request/auth.go
+++ backend/internal/dto/request/auth.go
@@ -1,12 +1,12 @@
 package request
 
 type SignUpRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
 }
