2ad2cfbf8cc360634b9274f245875a8d88f7f24d
やすす <yuta20253@yasusunoMBP.lan>
2026-06-30 07:21:15 +0900

    リポジトリ層をDDDに対応させる

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -3,40 +3,71 @@ package repository
 import (
 	"gorm.io/gorm"
 	"hobby-blog/internal/model"
+	"hobby-blog/internal/domain/user"
+	"context"
 )
-
-type UserRepository interface {
-	Create(user *model.User) error
-	FindByEmail(email string) (model.User, error)
-	FindByID(id uint) (model.User, error)
-}
 
 type userRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) UserRepository {
+func NewUserRepository(db *gorm.DB) user.UserRepository {
 	return &userRepository{db: db}
 }
 
-func (r *userRepository) Create(user *model.User) error {
-	return r.db.Create(user).Error
-}
-
-func (r *userRepository) FindByEmail(email string) (model.User, error) {
-	var user model.User
-	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
-		return model.User{}, err
+func (r *userRepository) FindByID(ctx context.Context, id user.ID) (*user.User, error) {
+	var m model.User
+	if err := r.db.WithContext(ctx).First(&m, uint(id)).Error; err != nil {
+		return nil, err
 	}
 
-	return user, nil
-}
-
-func (r *userRepository) FindByID(id uint) (model.User, error) {
-	var user model.User
-	if err := r.db.First(&user, id).Error; err != nil {
-		return model.User{}, err
+	name, err := user.NewName(m.Name)
+	if err != nil {
+		return nil, err
 	}
 
-	return user, nil
+	email, err := user.NewEmail(m.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return user.Reconstruct(
+		user.ID(m.ID),
+		name,
+		email,
+		m.PasswordHash,
+	), nil
+}
+
+func (r *userRepository) FindByEmail(ctx context.Context, email user.Email) (*user.User, error) {
+	var m model.User
+	if err := r.db.WithContext(ctx).Where("email = ?", email.String()).First(&m).Error; err != nil {
+		return nil, err
+	}
+
+	mail, err := user.NewEmail(m.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	name, err := user.NewName(m.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return user.Reconstruct(
+		user.ID(m.ID),
+		name,
+		mail,
+		m.PasswordHash,
+	), nil
+}
+
+func (r *userRepository) Create(ctx context.Context, u *user.User) error {
+	m := model.User{
+		Name: u.Name.String(),
+		Email: u.Email.String(),
+		PasswordHash: u.PasswordHash,
+	}
+	return r.db.WithContext(ctx).Create(&m).Error
 }
